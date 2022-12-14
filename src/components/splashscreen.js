44cf502a1d49db10002a85355a6797dd5535e2f2
joseda <josedhg29@gmail.com>
2022-12-13 21:23:07 -0300

    cambios

--- src/components/splashscreen.js
+++ src/components/splashscreen.js
@@ -1,11 +1,24 @@
 import React from "react";
-// import ContactHandler from "../controllers/contactHandler";
-// import useState from "react"
+
+import logo from '../imagenes/logo.png'
+import touch from '../imagenes/touch.png'
+import cerveza from '../imagenes/cerveza.png'
+import hamburguesa from '../imagenes/hamburguesas.png'
+import papas from '../imagenes/papas fritas.png'
+import pizza from '../imagenes/pizza.png'
+import fondo from '../imagenes/fondo.png'
 
 export function Splashscreen() {
   return (
     <>
-      <img src="../imagenes/logo.jpg" alt="" />
+      <img className="logo" src={logo} alt="" />
+      <img className="logo" src={touch} alt="" link="menuDisplay"/>
+      <img className="logo" src={cerveza} alt="" />
+      <img className="logo" src={hamburguesa} alt="" />
+      <img className="logo" src={papas} alt="" />
+      <img className="logo" src={pizza} alt="" />
+      <img className="logo" src={fondo} alt="" />
+
     </>
   );
 }
