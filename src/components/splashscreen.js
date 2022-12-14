9ad5202151ca5b684f692cd678541efd2cb7827a
luissilva1984 <luissilva.ortiz.co@gmail.com>
2022-12-14 17:51:10 -0300

    no puedo ajustar la pantalla francia a la final

--- src/components/splashscreen.js
+++ src/components/splashscreen.js
@@ -7,21 +7,24 @@ import hamburguesa from '../imagenes/hamburguesas.png'
 import papas from '../imagenes/papas fritas.png'
 import pizza from '../imagenes/pizza.png'
 import fondo from '../imagenes/fondo.png'
+import cachaña from '../imagenes/cachaña.png'
 import { Link } from "react-router-dom";
 
 export function Splashscreen() {
   return (
     
     <div>
-     <img className="" src={fondo} alt=""/>
+     <img className="fondo" src={fondo} alt=""/>
       <img className="logo" src={logo} alt="" />
       <Link id="touch" className="" to="/menuDisplay">
         <img className="touch" src={touch} alt="" />
+        <img className="cachaña" src={cachaña} alt="" />
       </Link>
       <img className="cerveza" src={cerveza} alt="" />
       <img className="hamburguesa" src={hamburguesa} alt="" />
       <img className="papas" src={papas} alt="" />
       <img className="pizza" src={pizza} alt="" />
+      
       </div>
       
     
