e525515110f628db13478bafe0681a7668c2777e
luissilva1984 <luissilva.ortiz.co@gmail.com>
2022-12-15 18:28:17 -0300

    vamos que se puede

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,32 +1,111 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+
 
 export function Sidebar() {
     return (
+        <div className="menu">
         <>
-            <ul>
-                <Link className="sidebarItem" to="/">
+        <div className="menuText">
+<Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Tragos</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Tragos</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Tablas</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Tablas</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link> 
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Pizzas</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Pizzas</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link> 
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Rolls</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Rolls</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link> 
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="4">
+        <Accordion.Header>Cafeteria</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Cafeteria</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link> 
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="5">
+        <Accordion.Header>Cervezas</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Cervezas</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="6">
+        <Accordion.Header>Vinos</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Vinos</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link>   
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="7">
+        <Accordion.Header>Aperitivos</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Aperitivos</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link>     
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="8">
+        <Accordion.Header>Ceviche</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Ceviche</li>
-                </Link>    <Link className="sidebarItem" to="/">
+                </Link>      
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="9">
+        <Accordion.Header>Almuerzo</Accordion.Header>
+        <Accordion.Body>
+        <Link className="sidebarItem" to="/">
                     <li>Almuerzo</li>
                 </Link>
-            </ul>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion> 
+    </div>    
         </>
-    );
+        </div>
+    
+   );
 }
+
+
+export default Sidebar;
