1a4bc2cd2e285edef9f6e80321b9bdbfa16b5442
ronald maldonado <wurtema@gmail.com>
2022-12-16 22:54:47 -0300

    vistadetallesactualizacion

--- src/components/vistaDetalle.js
+++ src/components/vistaDetalle.js
@@ -10,17 +10,26 @@ import pagar from '../imagenes/pagar.png'
 export function VistaDetalle() {
     return (
         <>
+        <div className="WWW">
+           <h3>Valor total: $</h3>
+           <img className="logoB" src={bandeja} alt="" /> 
+        </div>
+        <div className="KKK">
             <h1>Cervezas</h1>
             <h2>Marca Cerveza</h2>
+            <img className="logoC" src={cerveza} alt="" /> 
+        </div>  
+         <div className="QQQ">
             <p>Cantidad</p>
             <p>- 10 +</p>
             <p>Descripcion del Producto e Ingredientes</p>
-            <p>Precio: 3000$</p>     
-            <img className="logo" src={logo} alt="" />  
-            <img className="logo" src={bandeja} alt="" />  
-            <img className="logo" src={cerveza} alt="" />  
-            <img className="logo" src={observacion} alt="" />  
-            <img className="logo" src={pagar} alt="" />  
+            <p>Precio: 3000$</p> 
+        </div>
+           
+
+            <img className="logoA" src={logo} alt="" />  
+            <img className="logoD" src={observacion} alt="" />  
+            <img className="logoI" src={pagar} alt="" />  
              </>
     );
 }
